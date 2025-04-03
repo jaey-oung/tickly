76e9da5ba10ff2b886b4c5651dd3f5b68fd2f3c1
jaey-oung <jaeywodud7@gmail.com>
2025-04-03 13:13:22 +0900

    chore: React + Spring Boot 프로젝트 기본 환경 구성

---
+++ src/main/java/com/tickly/app/TicklyApplication.java
@@ -0,0 +1,13 @@
+package com.tickly.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicklyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicklyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tickly/app/TicklyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tickly.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicklyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
